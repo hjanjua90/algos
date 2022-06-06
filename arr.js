65f2cda0b58cc65426119b5482eb4b931ee8d2f6
hjanjua90 <harpreet_mann23@yahoo.com>
2022-06-06 14:19:04 -0700

    dupeArrays

--- arr.js
+++ arr.js
@@ -1,6 +1,5 @@
 // find the missing number in integer array of 1 to 100?
 
-
 // let arr = [1,2,3,4,5,7,8,9,10];
 // const findMissingNum = (arr) => {
 //     for(var i = 0; i < arr.length - 1; i++) {
@@ -12,6 +11,15 @@
 //     return false;
 // }
 // console.log(findMissingNum(arr));
+
+
+
+
+
+
+
+
+
 
 // finding duplicate numbers in array 
 
@@ -29,6 +37,13 @@
 
 // }
 // console.log(dupNum(arr))
+
+
+
+
+
+
+
 
 
 // finding the largest and smallest number in an unsorted array of integers
@@ -53,6 +68,13 @@
 // console.log(findMaxMin(arr))
 
 
+
+
+
+
+
+
+
 // Return an array showing the cumulative sum at each index of an array of integers
 
 // let arr = [1,3,5,7];
@@ -68,4 +90,97 @@
 // console.log(cummulativeSum(arr))
 
 
-// finding all duplicate numbers 
+
+
+
+
+
+
+// finding all duplicate numbers, if no dupes are found return empty array.
+
+// const arr = [1,1,2,3,4,5,6,7,8,6,6,7,7,10,10]
+
+// const returnMultipleDupesArray = (arr) =>{
+//     let observed = {};
+//     let dupesArray = [];
+
+//     for (let i = 0; i < arr.length; i++){
+//         if (observed[arr[i]]){
+//             if (observed [arr[i]] === 1){
+//                 dupesArray.push(arr[i]);
+//             }
+//             observed[arr[i]] = observed[arr[i]] + 1;
+//         }else {
+//             observed[arr[i]] = 1
+//         }
+//     }
+//     return dupesArray;
+// }
+
+// console.log(returnMultipleDupesArray(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+// removing all duplicates from an array of integers
+// goal return array with out multiples
+
+// const arr = [1,1,1,1];
+
+// const removeDupes = (arr) => {
+//     let result = [];
+//     let previous = arr[0];
+//     result [0] = previous;
+
+//     for(let i = 0; i< arr.length; i++){
+//         if(arr[i] != previous){
+//             result.push(arr[i]);
+//         }
+//         previous = arr[i];
+//     }
+//     return result;
+// }
+
+// console.log(removeDupes(arr))
+
+
+
+
+
+
+
+
+
+
+
+// Find all pairs in an array of integers whose sum is equal to a given number
+// return matching pairs or empty arrays
+
+let arr = [1,5,6,1,0,1];
+
+const findSumPairs = (arr, value) => {
+    let sumLookup = {};
+    let output = [];
+
+    for(let i=0; i < arr.length; i++){
+        let targetVal = value - arr[i];
+
+        if (sumLookup[targetVal]){
+            output.push([arr[i], targetVal]);
+        }
+        sumLookup[arr[i]] = true;
+    }
+    return output;
+}
+
+console.log(findSumPairs(arr, 6))
+
+
