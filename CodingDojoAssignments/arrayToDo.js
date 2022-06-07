a961c013cf904b39ae5ced0eee7d9e579f82f523
hjanjua90 <harpreet_mann23@yahoo.com>
2022-06-06 22:16:34 -0700

    adding Coding Dojo Assigment

---
+++ CodingDojoAssignments/arrayToDo.js
@@ -0,0 +1,22 @@
+// Remove Negatives
+// 1. remove negative calues and returns the same array with non-negatives.
+// use built in removeNegative()
+
+let arr = [1, -3,2,-10]
+const removeNegative = (arr) =>{
+    for (var i =0; i<= arr.length; i++)
+}
+
+
+
+// let arr = [1,2,3,4,5,7,8,9,10];
+// const findMissingNum = (arr) => {
+//     for(var i = 0; i < arr.length - 1; i++) {
+//         if(arr[i] + 1 != arr[i+1] ) {
+//             return arr[i] + 1;
+//     }
+//     }
+  
+//     return false;
+// }
+// console.log(findMissingNum(arr));
